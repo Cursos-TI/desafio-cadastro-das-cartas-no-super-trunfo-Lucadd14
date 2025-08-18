576677aa3b67e563e66f385c35d2d6eed12479dd
Lucadd14 <marcoslucassystem29@gmail.com>
2025-08-18 02:08:49 +0000

    cartas criadas

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,40 @@
 
 int main(){
 
+    printf("Carta01\n");
 
+    char estado;
+    char codigo;
+    char nomedacidade[100];
+    int populacao;
+    float area;
+    float pib;
+    int numerosdepontosturisticos;
+
+    printf("Qual o nome do estado: \n");
+    scanf("%s, &estado");
+
+    printf("Qual o codigo: \n");
+    scanf("%s, &codigo");
+
+    printf("")
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
 
 
 
