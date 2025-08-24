5a8e9b077a3b7fb2ce174c3ee48f0cebb48e2949
Lucadd14 <marcoslucassystem29@gmail.com>
2025-08-24 17:08:26 +0000

    carta criada

--- xadrez.c
+++ xadrez.c
@@ -4,19 +4,19 @@ int main(){
 
     printf("Carta01\n");
 
-    char estado[50];
-    char codigo;
-    char nomedacidade[100];
-    int populacao;
-    float area;
-    float pib;
-    int numerosdepontosturisticos;
+    char estado[50] = "amazonas";
+    char codigo[30] = "Am1";
+    char nomedacidade[100] = "manaus";
+    unsigned int populacao = "2300000";
+    char area[15]=" 2000000";
+    long int pib = "103500000";
+    int numerosdepontosturisticos = "50";
 
     printf("Qual o nome do estado: \n");
     scanf("%s", &estado);
 
     printf("Qual o codigo: \n");
-    scanf("%s", &codigo);
+    scanf("%s", &codigo);                            
 
     printf("Digite o nome da cidade: \n");
     scanf("%s", &nomedacidade);
@@ -24,20 +24,21 @@ int main(){
     printf("Qual a população: \n");
     scanf("%d", &populacao);
 
-    printf("Tamanho da area: \n");
-    scanf("%f", &area);
+    printf("Tamanho da area (KM2): \n");
+    scanf("%s",&area);
 
     printf("Qual o BIP: \n");
-    scanf("%f", &pib);
+    scanf("%ld", &pib);
 
     printf("Quantos pontos turisticos: \n");
     scanf("%d", &numerosdepontosturisticos);
 
-    printf("o nome do estado é: %s", estado, codigo);
-    printf("O nome da cidade é: %s - A população é de: %d\n", nomedacidade, populacao);
-    printf("o tamanho da area é: %f km - O PIB é de: %f\n", area, pib);
+    printf("o nome do estado é: %s - O código é: %s\n", estado, codigo);
+    printf("O nome da cidade é: %s - A população é de: %d (milhões)\n", nomedacidade, populacao);
+    printf("o tamanho da area é: %skm2 - O PIB é de: (R$) %ld\n", area, pib);
     printf("Numeros de pontos turisticos são de: %d\n", numerosdepontosturisticos);
 
     return 0;
 
 }
+
